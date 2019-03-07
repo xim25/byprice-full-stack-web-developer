764e0a23ef5c5f9835d165d5ac27237e62bfbad4
Sergio Morlan Paramo <serch.chk@gmail.com>
2019-03-07 14:57:21 -0600

    Add index

---
+++ public-src/index.js
@@ -0,0 +1,12 @@
+/*
+ * Your react code here
+  import React from 'react'
+  import { render } from 'react-dom'
+  import Users from './components/user'
+
+  const rootEl = document.getElementById('UserTodo')
+
+  render(<Users />, rootEl)
+
+  if (module.hot) module.hot.accept()
+*/
